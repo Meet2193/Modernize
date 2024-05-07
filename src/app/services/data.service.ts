d00c216e539b2c85b384f3ff549a4ef85d9c6c18
Meet2193 <meetnimavat170@gmail.com>
2024-05-07 17:58:20 +0530

    auto-generate-cookieHeader-7MAY

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { interval } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -9,14 +9,49 @@ import { switchMap } from 'rxjs/operators';
 export class DataService {
   http = inject(HttpClient);
   apiURL = 'https://fakestoreapi.com';
+  headers: HttpHeaders = new HttpHeaders(); // Initialize headers
 
-  constructor() {}
+  constructor() {
+    console.log('Initialize Service Filea');
 
+    this.fetchLatestHeadersAndCookies();
+    // Fetch headers and cookies immediately upon initialization
+  }
+
+  // getAllData() {
+  //   return this.http.get(`${this.apiURL}/products`);
+  // }
+
+  // fetchDataEveryMinute() {
+  //   return interval(30000).pipe(switchMap(() => this.getAllData()));
+  // }
+
+  // Method to fetch the latest headers and cookies from the third-party API
+  fetchLatestHeadersAndCookies() {
+    return this.http
+      .get(`${this.apiURL}/products`, { observe: 'response' })
+      .pipe(
+        tap((response) => {
+          // Assuming the response contains headers and cookies
+          console.log('response of headers', response);
+
+          this.headers = response.headers;
+          // Optionally, extract cookies if needed
+        })
+      );
+  }
+
+  // Adjusted getAllData to use the stored headers
   getAllData() {
-    return this.http.get(`${this.apiURL}/products`);
+    return this.http.get(`${this.apiURL}/products`, { headers: this.headers });
   }
 
   fetchDataEveryMinute() {
-    return interval(30000).pipe(switchMap(() => this.getAllData()));
+    return interval(60000).pipe(switchMap(() => this.getAllData()));
+  }
+
+  // Schedule to fetch the latest headers and cookies every 30 minutes
+  scheduleFetchHeadersAndCookies() {
+    interval(1800000).subscribe(() => this.fetchLatestHeadersAndCookies());
   }
 }
